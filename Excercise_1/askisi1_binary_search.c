ded8c4fc69633f66265ec52940b4d19466c78a36
Mike <19961621+MikeStavr@users.noreply.github.com>
2021-10-26 21:30:01 +0300

    Final version & added report document

--- Excercise_1/askisi1_binary_search.c
+++ Excercise_1/askisi1_binary_search.c
@@ -2,45 +2,43 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define N 20000
 
-#define N 1000
-
-int binarySearch(int array[], int x, int left, int right){
-	int mid;
-	int iterations = 0;
-	while (left <= right){
-		iterations += 1;
-		mid = left + (right - left) / 2;
-		if(array[mid] == x){
-			printf("\nFound %d after %d iterations", mid, iterations);
-			return iterations;
-		}if(array[mid] < x)
-			left = mid + 1;	
-		else
-			right = mid - 1;
-	}
+int binarySearch(int array[], int x, int left, int right) {
+  int mid;
+  int iterations = 0;
+  while (left <= right) {
+    iterations += 1;
+    mid = left + (right - left) / 2;
+    if (array[mid] == x) {
+      printf("\nFound %d after %d iterations", mid, iterations);
+      return iterations;
+    }
+    if (array[mid] < x)
+      left = mid + 1;
+    else
+      right = mid - 1;
+  }
 }
 
+int main() {
+  int pin[N], i, sum = 0;
+  int random, bSearch;
+  srand(time(NULL));
+  pin[0] = rand() % 100;
 
-int main(){
-	int pin[N], i, sum = 0;
-	int random, bSearch;
- 	 srand(time(NULL));
-    pin[0] = rand()%100;
+  for (i = 1; i < N; i++)
+    pin[i] = pin[i - 1] + rand() % 10;
 
-    for(i=1; i<N; i++)
-        pin[i] = pin[i-1] + rand()%10;
- 	
- 	for(i = 0; i < 10;i++){
- 		random = rand()%N;
- 		bSearch = binarySearch(pin, pin[random], 0, N-1);
- 		
- 		if(bSearch == -1){
- 			printf("\nNot found.");
-		 }
-		 
- 		sum = sum + bSearch;
-	}
- 	printf("\nAverage iterations: %d", sum/10);
- 		
+  for (i = 0; i < 10; i++) {
+    random = rand() % N;
+    bSearch = binarySearch(pin, pin[random], 0, N - 1);
+
+    if (bSearch == -1) {
+      printf("\nNot found.");
+    }
+
+    sum = sum + bSearch;
+  }
+  printf("\nAverage iterations: %d", sum / 10);
 }
--- Excercise_1/askisi1_ternary_search.c
+++ Excercise_1/askisi1_ternary_search.c
@@ -2,62 +2,60 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 10000
+#define N 20000
 
 int ternarySearch(int array[], int x, int left, int right) {
-    int mid1;
-    int mid2;
-    
-    int iters = 0;
+  int mid1;
+  int mid2;
 
-    while (left <= right) {
-        iters+=1;
-        mid1 = left + (right - left) / 3;
-        mid2 = right - (right - left) / 3;
-       
-        if (array[mid1] == x) {
-        	x = mid1;
-            break;
-        }
+  int iters = 0;
 
-        if (array[mid2] == x) {
-        	x = mid2;
-            break;
-            
-        }
-        if (array[mid1] > x) {
+  while (left <= right) {
+    iters += 1;
+    mid1 = left + (right - left) / 3;
+    mid2 = right - (right - left) / 3;
 
-            right = mid1 - 1;
-
-        } else if (array[mid2] < x) {
-
-            left = mid2 + 1;
-
-        } else {
-            left = mid1 + 1;
-            right = mid2 - 1;
-        }
+    if (array[mid1] == x) {
+      x = mid1;
+      break;
     }
-    printf("\nRequired %d iterations for number at slot %d", iters, x);
-    return iters;
+
+    if (array[mid2] == x) {
+      x = mid2;
+      break;
+    }
+    if (array[mid1] > x) {
+
+      right = mid1 - 1;
+
+    } else if (array[mid2] < x) {
+
+      left = mid2 + 1;
+
+    } else {
+      left = mid1 + 1;
+      right = mid2 - 1;
+    }
+  }
+  printf("\nRequired %d iterations for number at slot %d", iters, x);
+  return iters;
 }
 
 int main() {
-    
-	int pin[N], i, avg=0, sum=0;
 
-     srand(time(NULL));
-     pin[0] = rand()%100;
-     
-	for(i=1;i<N;i++)
-		pin[i] = pin[i-1] + rand()%10;
+  int pin[N], i, avg = 0, sum = 0;
 
+  srand(time(NULL));
+  pin[0] = rand() % 100;
 
-    for (i = 0; i < 10; i++) {
-        int toSearch = rand() % N;
-        int iteration = ternarySearch(pin, pin[toSearch], 0, N-1);
-        sum = sum + iteration;
-    }
-    avg = sum / 10;
-    printf("\nAverage %d comparisons for %d data.", avg, N);
+  for (i = 1; i < N; i++)
+    pin[i] = pin[i - 1] + rand() % 10;
+
+  for (i = 0; i < 10; i++) {
+    int toSearch = rand() % N;
+    int iteration = ternarySearch(pin, pin[toSearch], 0, N - 1);
+    sum = sum + iteration;
+  }
+  avg = sum / 10;
+  printf("\nAverage %d comparisons for %d data.", avg, N);
 }
