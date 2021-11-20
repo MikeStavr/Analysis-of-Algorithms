69555c6f329c1092b99f7b8b97af5c9e04ce438b
Mike <19961621+MikeStavr@users.noreply.github.com>
2021-11-20 16:20:02 +0200

    Excercise finished - last

--- Excercise_3/main.c
+++ Excercise_3/main.c
@@ -7,6 +7,12 @@
 #define M 25000
 #define K 50000
 
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
 void merge(int pin[], int left, int mid, int right) {
   int new = mid + 1;
   if (pin[mid] <= pin[new]) {
@@ -41,22 +47,22 @@ void mergeSort(int pin[], int left, int right) {
 }
 
 int partition(int pin[], int left, int right) {
-  int pivot = right;
-  int store = left - 1;
-  int i;
-  for (i = left; i < right; i++) {
-    if (pin[i] <= pin[pivot]) {
-      int temp = pin[i];
-      pin[i] = pin[store];
-      pin[store] = temp;
+
+  int pivot = pin[right];
+  int store = (left - 1);
+  int j;
+
+  for (j = left; j < right; j++) {
+    if (pin[j] <= pivot) {
       store++;
+      swap(&pin[store], &pin[j]);
     }
   }
-  int temp2 = pin[pivot];
-  pin[pivot] = pin[store];
-  pin[store] = temp2;
-  return store + 1;
+
+  swap(&pin[store + 1], &pin[right]);
+  return (store + 1);
 }
+
 void quicksort(int pin[], int left, int right) {
   int pivot;
   if (left < right) {
@@ -73,63 +79,78 @@ void generateRandomArray(int pin[], int number) {
     pin[i] = randomNumber;
   }
 }
+void emptyArray(int pin[], int number) {
+  int i;
+  for (i = 0; i < number; i++) {
+    pin[i] = 0;
+  }
+}
 
 int main() {
   int pin1[N], pin2[M], pin3[K];
   int i;
   double average1, average2, average3, average4, average5, average6;
+  double time_taken;
+  clock_t start;
 
+  printf("\nBefore for < 10\n");
   for (i = 0; i < 10; i++) {
     generateRandomArray(pin1, N);
     generateRandomArray(pin2, M);
     generateRandomArray(pin3, K);
 
-    clock_t start1 = clock();
+    printf("\nMerge sort 1\n");
+    start = clock();
     mergeSort(pin1, 0, N - 1);
-    clock_t end1 = clock();
-    double time1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
-    average1 += time1;
-
-    clock_t start2 = clock();
+    start = clock() - start;
+    time_taken = (double)(start) / CLOCKS_PER_SEC;
+    average1 += time_taken;
+    printf("\nMerge sort 2\n");
+    start = clock();
     mergeSort(pin2, 0, M - 1);
-    clock_t end2 = clock();
-    double time2 = (double)(end2 - start2) / CLOCKS_PER_SEC;
-    average2 += time2;
-
-    clock_t start3 = clock();
+    start = clock() - start;
+    time_taken = (double)(start) / CLOCKS_PER_SEC;
+    average2 += time_taken;
+    printf("\nMerge sort 3\n");
+    start = clock();
     mergeSort(pin3, 0, K - 1);
-    clock_t end3 = clock();
-    double time3 = (double)(end3 - start3) / CLOCKS_PER_SEC;
-    average3 += time3;
+    start = clock() - start;
+    time_taken = (double)(start) / CLOCKS_PER_SEC;
+    average3 += time_taken;
 
     generateRandomArray(pin1, N);
     generateRandomArray(pin2, M);
     generateRandomArray(pin3, K);
 
-    clock_t start4 = clock();
+    printf("\nQuick sort 1\n");
+    start = clock();
     quicksort(pin1, 0, N - 1);
-    clock_t end4 = clock();
-    double time4 = (double)(end4 - start4) / CLOCKS_PER_SEC;
-    average4 += time4;
+    start = clock() - start;
+    time_taken = (double)(start) / CLOCKS_PER_SEC;
+    average4 += time_taken;
 
-    clock_t start5 = clock();
+    printf("\nQuick sort 2\n");
+    start = clock();
     quicksort(pin2, 0, M - 1);
-    clock_t end5 = clock();
-    double time5 = (double)(end5 - start5) / CLOCKS_PER_SEC;
-    average5 += time5;
+    start = clock() - start;
+    time_taken = (double)(start) / CLOCKS_PER_SEC;
+    average5 += time_taken;
 
-    clock_t start6 = clock();
+    printf("\nQuick sort 3\n");
+    start = clock();
     quicksort(pin3, 0, K - 1);
-    clock_t end6 = clock();
-    double time6 = (double)(end6 - start6) / CLOCKS_PER_SEC;
-    average6 += time6;
+    start = clock() - start;
+    time_taken = (double)(start) / CLOCKS_PER_SEC;
+    average6 += time_taken;
   }
 
   printf("Merge sort:\n");
   printf("pin1 = %f", average1 / 10);
   printf("\npin2 = %f", average2 / 10);
   printf("\npin3 = %f", average3 / 10);
+
   printf("\nQuick sort:\n");
+
   printf("pin1 = %f", average4 / 10);
   printf("\npin2 = %f", average5 / 10);
   printf("\npin3 = %f", average6 / 10);
