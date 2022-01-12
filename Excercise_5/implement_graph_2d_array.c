bfc17c70a720f0cd85d6f69d5be6880b9f1ca7ee
Mike <19961621+MikeStavr@users.noreply.github.com>
2022-01-12 21:27:00 +0200

    excercise 1 of ex5 finished

--- Excercise_5/implement_graph_2d_array.c
+++ Excercise_5/implement_graph_2d_array.c
@@ -2,36 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define vertices 4
-
-void setGraphMatrix(int mat[][vertices]) {
+void fillGraph(int **mat, int v1, int v2) {
   int i, j;
-  for (i = 0; i < vertices; i++) {
-    for (j = 0; j < vertices; j++) {
+  for (i = 0; i < v1; i++) {
+    for (j = 0; j < v2; j++) {
       mat[i][j] = 0;
     }
   }
 }
-
-void addEdge(int mat[][vertices], int i, int j) {
-  mat[i][j] = 1;
-  mat[j][i] = 1;
-}
-
-void print(int mat[][vertices]) {
-  int i, j;
-  for (i = 0; i < vertices; i++) {
-    printf("\n");
-    for (j = 0; j < vertices; j++) {
-      printf("%d ", mat[i][j]);
-    }
-  }
+void addEdge(int **mat, int m, int n) {
+  mat[m][n] = 1;
+  mat[n][m] = 1;
 }
 
 int main() {
-  int mat[vertices][vertices];
+  int v;
+  printf("Input how many vertices there will be: ");
+  scanf("%d", &v);
+  int **mat = (int **)malloc(v * sizeof(int *));
+  int i, j;
+  for (i = 0; i < v; i++) {
+    mat[i] = (int *)malloc(v * sizeof(int));
+  }
 
-  setGraphMatrix(mat);
+  fillGraph(mat, v, v);
   addEdge(mat, 0, 1);
   addEdge(mat, 0, 2);
   addEdge(mat, 1, 2);
@@ -39,5 +33,18 @@ int main() {
   addEdge(mat, 2, 3);
   addEdge(mat, 3, 3);
 
-  print(mat);
+  // Printing the array
+  for (i = 0; i < v; i++) {
+    for (j = 0; j < v; j++) {
+      printf("%3d ", mat[i][j]);
+    }
+    printf("\n");
+  }
+  // Freeing the space of the array
+  for (i = 0; i < v; i++) {
+    free(mat[i]);
+  }
+  // Free the array out of the memory
+  free(mat);
+  return 0;
 }
